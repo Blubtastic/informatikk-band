d635ed7367290b1c1f2cb34748bafca7c85fe770
Martin Storødegård <martin.storodegard@gmail.com>
2018-04-23 22:45:23 +0200

    created simple navbar and cover

---
+++ informatikk-band/src/components/Home/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import Nav from '../Nav'
+
+import band from "../../images/band.jpg"
+
+export default class Home extends Component {
+
+  render() {
+    return(
+      <div>
+        <Nav/>
+        <div className="cover-img">
+          <h1>Velkommen til Online sitt feteste linjeband!</h1>
+        </div>
+        <div className="content">
+        </div>
+      </div>
+    )
+  }
+}
